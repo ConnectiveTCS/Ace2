7b87fa42ec920cb551592a23b3fcd3938426de80
ConnectiveTCS <kyle@acewebdesign.co.za>
2025-02-27 08:53:41 +0400

    Enhance project card view to conditionally display client rating and improve layout styling

--- resources/views/components/project-card.blade.php
+++ resources/views/components/project-card.blade.php
@@ -4,14 +4,22 @@
         {{-- Project Image --}}
         <img src="{{ $project->clientImage ?? asset('assets/ND1.webp') }}"
             alt="{{ $project->clientName ?? 'Client Name' }} Photo" class="w-full h-96 object-cover rounded-lg shadow-lg">
+
+            {{-- If the project does not have client rating comment then do not show --}}
+        @if ($project->clientRating)
         {{-- Review Section --}}
         <h3 class="text-xl font-bold dark:text-white text-black">Client Review</h3>
         <div class="flex flex-col items-start justify-center space-y-4 p-2 bg-gray-100 dark:bg-gray-900 rounded-lg">
             {{-- Rating --}}
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EAC452">
-                <path
-                    d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-350Z" />
-            </svg>
+            {{-- Multiply the amount of svg stars according to the client's rating --}}
+            <div class="flex items-center space-x-2 text-black">
+            @for ($i = 0; $i < $project->clientRating; $i++)
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EAC452">
+                    <path
+                        d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-350Z" />
+                </svg>
+            @endfor
+            </div>
             {{-- Client Review Comment --}}
             <p class="text-sm my-2 font-thin dark:text-white text-black">
                 {{ $project->clientReviewProject ?? 'My website is what I have ask for and more. If you want a practical Website and good value for your money , I highly recommend Ace Web Design.' }}
@@ -27,16 +35,17 @@
                 </p>
             </div>
         </div>
+        @endif
     </div>
     {{-- Right Column --}}
-    <div class="border-t border-red-600 sticky top-24 self-start end gap-4 flex flex-row p-4">
+    <div class=" border-t-2 border-[#5ecde1] sticky top-24 self-start end gap-4 flex flex-row p-4">
         {{-- Project Details --}}
         <div class="md:block md:min-w-[20%] dark:text-white text-black">
             {{ $project->clientProjectDuration ?? '1 Month' }}
         </div>
         <div class="md:w-[80%]">
             <h2 class="text-3xl dark:text-white text-black">{{ $project->clientBusinessName ?? 'Next Devlivery' }}</h2>
-            <hr class="my-4 border-red-600">
+            <hr class="my-4 border-[#5ecde1]">
             <p class="dark:text-white text-black">
                 @if (is_array($project->clientShortStudy))
                     {{ implode(', ', $project->clientShortStudy) }}
